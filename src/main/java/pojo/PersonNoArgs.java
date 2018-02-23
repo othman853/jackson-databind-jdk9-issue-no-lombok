d62360cb22cf8d8ffa9a306e9b7abedc35510a9c
othman853 <anuar.yasser@gmail.com>
2018-02-23 12:10:34 -0300

    No Lombok. Problem persists

---
+++ src/main/java/pojo/PersonNoArgs.java
@@ -0,0 +1,17 @@
+package pojo;
+
+public class PersonNoArgs {
+
+    private String name;
+    private Integer age;
+
+    public PersonNoArgs() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+}
